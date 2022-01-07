304b586b527949edc22f9b4fd3d80a468f1fc467
seca.team <seca.app.team@gmail.com>
2022-01-07 21:49:37 +0530

    u1

--- app/src/main/java/com/example/arachnid/MainActivity.java
+++ app/src/main/java/com/example/arachnid/MainActivity.java
@@ -7,12 +7,18 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
+
+    SearchView Search_View;
+    TextView searchViewText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,18 @@ public class MainActivity extends AppCompatActivity {
 
         // to make the Navigation drawer icon always appear on the action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Search_View = findViewById(R.id.searchView);
+        searchViewText = findViewById(R.id.searchViewText);
+
+
+        Search_View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchViewText.setVisibility(View.INVISIBLE);
+
+            }
+        });
 
     }
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {  // To Open 3 line icon for navigation
