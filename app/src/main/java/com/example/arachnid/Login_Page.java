0d2c5087ff96687761c123c29966f7b16ceb2ebd
seca.team <seca.app.team@gmail.com>
2022-01-07 20:22:01 +0530

    u1

--- app/src/main/java/com/example/arachnid/Login_Page.java
+++ app/src/main/java/com/example/arachnid/Login_Page.java
@@ -3,9 +3,11 @@ package com.example.arachnid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +16,10 @@ import com.google.android.material.textfield.TextInputLayout;
 
 public class Login_Page extends AppCompatActivity {
     TextInputEditText username_tf, password_tf;
+    Button login_btn;
+
+
+    String username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,33 @@ public class Login_Page extends AppCompatActivity {
 
         username_tf = findViewById(R.id.id_username);
         password_tf = findViewById(R.id.id_password);
+        login_btn = findViewById(R.id.login_btn_id);
+
+        username = username_tf.getText().toString();
+        password = password_tf.getText().toString();
+        //username = username_tf.getText();
+
+        // username and password, confirm value from sql, after connecting with MySQL, Azure.
+
+        {
+            login_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    /*if(username.equals("seca") && password.equals("123")) {
+                        Toast.makeText(getApplicationContext(), "Login Successful.", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Incorrect username/password.", Toast.LENGTH_SHORT).show();
+                    }*/
+                    //Toast.makeText(getApplicationContext(), username + " " + password, Toast.LENGTH_SHORT).show();
+                    Log.e("Login_Page", username + password);
+                }
+            });
+
+        }
+
+
+
 
 
     }
