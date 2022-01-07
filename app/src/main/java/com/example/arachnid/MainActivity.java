7d167f1aa61da5e711d736e2db470326d7028948
seca.team <seca.app.team@gmail.com>
2022-01-07 18:13:49 +0530

    u1

--- app/src/main/java/com/example/arachnid/MainActivity.java
+++ app/src/main/java/com/example/arachnid/MainActivity.java
@@ -8,11 +8,5 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // a man code
-        // SECOND CHANGE
-        //evan made changes
-        //tHIRD cHANGE
-        //FOURTH CHANGE
-        // fifth change
     }
 }
