19bf423e680b858a0fa61782533b0c297388a10e
seca.team <seca.app.team@gmail.com>
2022-01-08 16:36:48 +0530

    08.01.2022

--- app/src/main/java/com/example/arachnid/Login_Page.java
+++ app/src/main/java/com/example/arachnid/Login_Page.java
@@ -3,11 +3,9 @@ package com.example.arachnid;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,7 +13,7 @@ import com.google.android.material.textfield.TextInputLayout;
 
 public class Login_Page extends AppCompatActivity {
     TextInputLayout username_tf, password_tf;
-    Button login_btn;
+    Button login_cont_btn, signup_btn;
 
     String username, password;
 
@@ -26,30 +24,51 @@ public class Login_Page extends AppCompatActivity {
         getSupportActionBar().hide();   // hide the title bar
         setContentView(R.layout.login_page);
 
-        username_tf = findViewById(R.id.id_username_layout);
-        password_tf = findViewById(R.id.id_password_layout);
-        login_btn = findViewById(R.id.login_btn_id);
+
 
         // username and password, confirm value from sql, after connecting with MySQL, Azure.
 
-        {
-            login_btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
+        username_tf = findViewById(R.id.id_username_layout);
+        password_tf = findViewById(R.id.id_password_layout);
+        login_cont_btn = findViewById(R.id.login_cont_btn_id);
+        signup_btn = findViewById(R.id.signup_btn_id);
 
-                    username = username_tf.getEditText().getText().toString();
-                    password = password_tf.getEditText().getText().toString();
 
-                    if(username.equals("seca") && password.equals("123")) {
-                        Toast.makeText(getApplicationContext(), "Welcome, " + username, Toast.LENGTH_SHORT).show();
-                    }else{
-                        Toast.makeText(getApplicationContext(), "Incorrect username/password.", Toast.LENGTH_SHORT).show();
-                    }
+        login_cont_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                username = username_tf.getEditText().getText().toString();
+                password = password_tf.getEditText().getText().toString();
+
+                // if username and password are correct
+                if(username.equals("seca") && password.equals("123")) {
+                    Toast.makeText(getApplicationContext(), "Welcome, " + username, Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(Login_Page.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }else{
+                    Toast.makeText(getApplicationContext(), "Incorrect username/password.", Toast.LENGTH_SHORT).show();
                 }
-            });
 
-        }
+            }
+        });
+
+
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Login_Page.this, Signup_Page.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+
 
 
 
--- app/src/main/java/com/example/arachnid/Signup_Page.java
+++ app/src/main/java/com/example/arachnid/Signup_Page.java
@@ -1,18 +1,35 @@
 package com.example.arachnid;
+
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class Signup_Page extends AppCompatActivity {
+
+    Button signup_cont_btn, login_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
         getSupportActionBar().hide(); // hide the title bar
-        // this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-        //WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.signup_page);
+
+        signup_cont_btn = findViewById(R.id.signup_cont_btn_id);
+        login_btn = findViewById(R.id.login_btn_id);
+
+        signup_cont_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
 
     }
 
