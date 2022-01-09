b2f2e725f0380a7a189a7bb519ff0d732b41adc9
seca.team <seca.app.team@gmail.com>
2022-01-09 18:52:51 +0530

    09.01.2022

--- app/src/main/java/com/example/arachnid/MainActivity.java
+++ app/src/main/java/com/example/arachnid/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.arachnid;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
